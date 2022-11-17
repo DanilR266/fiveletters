71b34e48809759429bd9bed31076b406fa11a7a0
DanilR266 <danilwind8@gmail.com>
2022-11-17 14:55:03 +0300

    Initial Commit

---
+++ fiveletters/fivelettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  fivelettersApp.swift
+//  fiveletters
+//
+//  Created by Данила on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct fivelettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
