f70dbe1b1efc0ec33dff916f9f9800664552e8ce
DanilR266 <danilwind8@gmail.com>
2023-01-14 17:12:23 +0300

    commit

--- fiveletters/FIeldView.swift
+++ fiveletters/FIeldView.swift
@@ -51,6 +51,26 @@ struct FIeldView: View {
     }
 }
 
+struct Field: View {
+    @EnvironmentObject var dm: Model
+    var letter: String
+    var color: Color = .white
+    var body: some View {
+        Text(letter)
+                    .frame(width: 45, height: 54, alignment: .center)
+                    .font(.system(size: 28, weight: .light, design: .default))
+                    .foregroundColor(.black)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 3)
+                            .stroke(Color(.sRGB, red: 255/255, green: 234/255, blue: 45/255, opacity: 1), lineWidth: 5)
+                )
+            .background(color)
+            .cornerRadius(3)
+            .frame(width: 45, height: 54, alignment: .center)
+
+    }
+}
+
 struct FIeldView_Previews: PreviewProvider {
     static var previews: some View {
         FIeldView()
--- fiveletters/StartGame.swift
+++ fiveletters/StartGame.swift
@@ -11,25 +11,25 @@ import SwiftUI
 
 
 
-struct Field: View {
-    @EnvironmentObject var dm: Model
-    var letter: String
-    var color: Color = .white
-    var body: some View {
-        Text(letter)
-                    .frame(width: 45, height: 54, alignment: .center)
-                    .font(.system(size: 28, weight: .light, design: .default))
-                    .foregroundColor(.black)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 3)
-                            .stroke(Color(.sRGB, red: 255/255, green: 234/255, blue: 45/255, opacity: 1), lineWidth: 5)
-                )
-            .background(color)
-            .cornerRadius(3)
-            .frame(width: 45, height: 54, alignment: .center)
-
-    }
-}
+//struct Field: View {
+//    @EnvironmentObject var dm: Model
+//    var letter: String
+//    var color: Color = .white
+//    var body: some View {
+//        Text(letter)
+//                    .frame(width: 45, height: 54, alignment: .center)
+//                    .font(.system(size: 28, weight: .light, design: .default))
+//                    .foregroundColor(.black)
+//                    .overlay(
+//                        RoundedRectangle(cornerRadius: 3)
+//                            .stroke(Color(.sRGB, red: 255/255, green: 234/255, blue: 45/255, opacity: 1), lineWidth: 5)
+//                )
+//            .background(color)
+//            .cornerRadius(3)
+//            .frame(width: 45, height: 54, alignment: .center)
+//
+//    }
+//}
 
 
 //struct StartGame_Previews: PreviewProvider {
